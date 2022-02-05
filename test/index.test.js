73a0bbd6d7c96c3ebb8c3a30c27311b1a72b545b
C. Lewis <ctj.lewis@icloud.com>
2022-02-05 14:13:34 -0600

    chore: error code ENOENT now errNo -2

--- test/index.test.js
+++ test/index.test.js
@@ -247,7 +247,7 @@ run.methods.forEach((method) => {
                 expect(err.message).toMatch('ENOENT');
                 expect(err.message).not.toMatch('undefined');
                 expect(err.code).toBe('ENOENT');
-                expect(err.errno).toBe('ENOENT');
+                expect(err.errno).toBe(-2);
                 expect(err.syscall).toMatch(syscall);
                 expect(err.syscall).not.toMatch('undefined');
                 expect(err.path).toMatch('somecommandthatwillneverexist');
